ffe70e59f31640175e3ba96fe62b46b9ee678e61
ass782 <ass782@student.bham.ac.uk>
2018-02-15 23:36:47 +0000

    Reformatted code indentations

--- src/ClientReceiver.java
+++ src/ClientReceiver.java
@@ -8,44 +8,43 @@ import java.net.SocketException;
 
 public class ClientReceiver extends Thread {
 
-  private BufferedReader server;
+	private BufferedReader server;
 
-  ClientReceiver(BufferedReader server) {
-    this.server = server;
-  }
+	ClientReceiver(BufferedReader server) {
+		this.server = server;
+	}
 
-  /**
-   * Run the client receiver thread.
-   */
-  public void run() {
-    // Print to the user whatever we get from the server:
-    try {
-      while (true) {
-        String s = server.readLine(); // Matches FFFFF in ServerSender.java
+	/**
+	 * Run the client receiver thread.
+	 */
+	public void run() {
+		// Print to the user whatever we get from the server:
+		try {
+			while (true) {
+				String s = server.readLine(); // Matches FFFFF in ServerSender.java
 
-        if (s == null) {
-          throw new NullPointerException();
-        }
+				if (s == null) {
+					throw new NullPointerException();
+				}
 
-        System.out.println(s);
-      }
-    } catch (SocketException e) { // Matches HHHHH in Client.java
-      Report.behaviour("Client receiver ending");
-    } catch (NullPointerException | IOException e) {
-      Report.errorAndGiveUp("Server seems to have died "
-              + (e.getMessage() == null ? "" : e.getMessage()));
-    }
-  }
+				System.out.println(s);
+			}
+		} catch (SocketException e) { // Matches HHHHH in Client.java
+			Report.behaviour("Client receiver ending");
+		} catch (NullPointerException | IOException e) {
+			Report.errorAndGiveUp("Server seems to have died " + (e.getMessage() == null ? "" : e.getMessage()));
+		}
+	}
 }
 
-
 /*
-
+ * 
  * The method readLine returns null at the end of the stream
-
+ * 
  * It may throw IoException if an I/O error occurs
-
- * See https://docs.oracle.com/javase/8/docs/api/java/io/BufferedReader.html#readLine--
-
-
+ * 
+ * See https://docs.oracle.com/javase/8/docs/api/java/io/BufferedReader.html#
+ * readLine--
+ * 
+ * 
  */
--- src/Server.java
+++ src/Server.java
@@ -1,3 +1,4 @@
+
 // Usage:
 //        java Server
 //
@@ -13,53 +14,52 @@ import java.net.Socket;
 
 public class Server {
 
-  /**
-   * Start the server listening for connections.
-   */
-  public static void main(String [] args) {
+	/**
+	 * Start the server listening for connections.
+	 */
+	public static void main(String[] args) {
 
-    // This table will be shared by the server threads:
-    ClientTable clientTable = new ClientTable();
-    
-    ServerSocket serverSocket = null;
-    
-    try {
-      serverSocket = new ServerSocket(Port.number);
-    } catch (IOException e) {
-      Report.errorAndGiveUp("Couldn't listen on port " + Port.number);
-    }
-    
-    try { 
-      // We loop for ever, as servers usually do.
-      while (true) {
-        // Listen to the socket, accepting connections from new clients:
-        Socket socket = serverSocket.accept(); // Matches AAAAA in Client
+		// This table will be shared by the server threads:
+		ClientTable clientTable = new ClientTable();
 
-        // This is so that we can use readLine():
-        BufferedReader fromClient = new BufferedReader(
-            new InputStreamReader(socket.getInputStream()));
+		ServerSocket serverSocket = null;
 
-        // We ask the client what its name is:
-        String clientName = fromClient.readLine(); // Matches BBBBB in Client
+		try {
+			serverSocket = new ServerSocket(Port.number);
+		} catch (IOException e) {
+			Report.errorAndGiveUp("Couldn't listen on port " + Port.number);
+		}
 
-        Report.behaviour(clientName + " connected");
-        
-        // We add the client to the table:
-        clientTable.add(clientName);
+		try {
+			// We loop for ever, as servers usually do.
+			while (true) {
+				// Listen to the socket, accepting connections from new clients:
+				Socket socket = serverSocket.accept(); // Matches AAAAA in Client
 
-        // We create and start a new thread to write to the client:
-        PrintStream toClient = new PrintStream(socket.getOutputStream());
-        ServerSender serverSender = new ServerSender(clientTable.getQueue(clientName), toClient);
-        serverSender.start();
+				// This is so that we can use readLine():
+				BufferedReader fromClient = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-        // We create and start a new thread to read from the client:
-        (new ServerReceiver(clientName, fromClient, clientTable, serverSender)).start();
-      }
-    } catch (IOException e) {
-      // Lazy approach:
-      Report.error("IO error " + e.getMessage());
-      // A more sophisticated approach could try to establish a new
-      // connection. But this is beyond the scope of this simple exercise.
-    }
-  }
+				// We ask the client what its name is:
+				String clientName = fromClient.readLine(); // Matches BBBBB in Client
+
+				Report.behaviour(clientName + " connected");
+
+				// We add the client to the table:
+				clientTable.add(clientName);
+
+				// We create and start a new thread to write to the client:
+				PrintStream toClient = new PrintStream(socket.getOutputStream());
+				ServerSender serverSender = new ServerSender(clientTable.getQueue(clientName), toClient);
+				serverSender.start();
+
+				// We create and start a new thread to read from the client:
+				(new ServerReceiver(clientName, fromClient, clientTable, serverSender)).start();
+			}
+		} catch (IOException e) {
+			// Lazy approach:
+			Report.error("IO error " + e.getMessage());
+			// A more sophisticated approach could try to establish a new
+			// connection. But this is beyond the scope of this simple exercise.
+		}
+	}
 }
