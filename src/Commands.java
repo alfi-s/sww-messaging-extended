adbe20e1c385e69449fa4a66aca9e997111073bf
ass782 <ass782@student.bham.ac.uk>
2018-02-15 23:31:11 +0000

    Initial commit

---
+++ src/Commands.java
@@ -0,0 +1,13 @@
+public class Commands {
+    public static final String REGISTER = "register";
+    public static final String LOGIN    = "login";
+    public static final String LOGOUT   = "logout";
+    public static final String SEND     = "send";
+    public static final String PREV     = "previous";
+    public static final String NEXT     = "next";
+    public static final String DELETE   = "delete";
+
+    public static final String USER_ALREADY_EXISTS = "That username already exists";
+    public static final String USER_NOT_FOUND      = "That username does not exist, please register";
+    public static final String CONNECTION_SUCCESS  = "Succeeded to connect";
+}
